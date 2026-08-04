967184e98c81f0ab10dd88387d5e2145e03965d3
Ethan Stokes <erstokes10@gmail.com>
2026-08-04 10:31:30 -0500

    both mavens

--- plugins/helper/src/main/kotlin/com/possible_triangle/gradle/settings/GradleHelperSettingsPlugin.kt
+++ plugins/helper/src/main/kotlin/com/possible_triangle/gradle/settings/GradleHelperSettingsPlugin.kt
@@ -17,6 +17,9 @@ class GradleHelperSettingsPlugin : Plugin<Settings> {
         target.pluginManagement {
             repositories {
                 maven {
+                    url = URI("https://registry.somethingcatchy.net/repository/maven-public/")
+                }
+                maven {
                     url = URI("https://maven.frozenblock.net/snapshot")
                 }
             }
